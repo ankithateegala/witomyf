dab0043c365b9574796fbbba3f43a26d94c96166
ankitha <ankitha.rao.t@gmail.com>
2017-02-12 00:41:22 -0600

    controller, model

---
+++ witomyf.API/Controllers/WitomyfController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net;
+using witomyf.API.Models;
+
+namespace witomyf.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class WitomyfController : Controller
+    {
+        //GET api/witomyf
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/witomyf/5
+        [HttpGet("{day}")]
+        public Witomyf Get(string day)
+        {
+            return new Witomyf { Day = day, Eat1 = 50 };
+        }
+
+        // POST api/witomyf
+        [HttpPost]
+        public void Post([FromBody]Witomyf witomyf)
+        {
+
+        }
+
+        // PUT api/witomyf/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        // DELETE api/witomyf/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ witomyf.API/Models/Witomyf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace witomyf.API.Models
+{
+    public class Witomyf
+    {
+        public string Day;
+        public int Eat1;
+        public int Eat2;
+        public int Eat3;
+        public int Eat4;
+        public int Eat5;
+        public int Eat6;
+    }
+}
