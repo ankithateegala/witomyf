549b2c4c2c7292a2bb7fd261836a51f5e96cb044
ankitha <ankitha.rao.t@gmail.com>
2017-02-11 22:27:54 -0600

    new model, controller

---
+++ witomyf/Witomyf.API/Models/Witomyf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Witomyf.API.Models
+{
+    public class Witomyf
+    {
+        public String Day;
+        public int Eat1;
+        public int Eat2;
+        public int Eat3;
+        public int Eat4;
+        public int Eat5;
+        public int Eat6;
+    }
+}
